97463b31bbd8d01bc45ad29671f41bcb0f267eeb
atenghamn <alex.tenghamn@gmail.com>
2021-11-16 21:34:12 +0100

    built the API-call function

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const apiKey = "c99451cb5566760aea5851ed0e24d5f4"
+
 let cityName = "London";
 
-let weatherMap = {}; 
+async function getWeather(name) {
 
-async function getWeather() {
-    const res = await fetch (`api.openweathermap.org/data/2.5/weather?q={${cityName}}&appid={${apiKey}}`);
+    const cityName = name;
+
+    const res = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=c99451cb5566760aea5851ed0e24d5f4`);
     const data = await res.json(); 
 
-    console.log(data);
 
-    weatherMap = data; 
-    console.log(weatherMap);
+   const weatherStation = {
+        temp:  data.main.temp,
+        feelsLike:  data.main.feels_like,
+        humidity: data.main.humidity,
+        description: data.weather[0].main,
+        subDescription: data.weather[0].description,
+        windSpeed: data.wind.speed,
+        icon: data.weather[0].icon
+    }
+
+    return weatherStation;
 }
-getWeather();
+
+const paris = getWeather("Paris");
+
+
